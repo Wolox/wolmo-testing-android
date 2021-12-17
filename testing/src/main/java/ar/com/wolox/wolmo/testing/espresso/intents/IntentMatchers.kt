5a927c1e45c03594b3955c0a57bd7570a410595b
ffasitella <federico.d.fasitella@accenture.com>
2021-12-17 11:23:45 -0300

    Added new intent matcher

--- testing/src/main/java/ar/com/wolox/wolmo/testing/espresso/intents/IntentMatchers.kt
+++ testing/src/main/java/ar/com/wolox/wolmo/testing/espresso/intents/IntentMatchers.kt
@@ -1,14 +1,31 @@
 package ar.com.wolox.wolmo.testing.espresso.intents
 
+import android.app.Instrumentation
+import android.content.Intent
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
+import org.hamcrest.Matchers
 
 object IntentMatchers {
     fun checkIntent(className: String, action: () -> Unit) {
         Intents.init()
         action()
         intended(hasComponent(className))
+        Intents.release()
+    }
+
+    fun checkIntentWithActionView(uri: String, action: () -> Unit) {
+        Intents.init()
+        val intent = Matchers.allOf(
+            IntentMatchers.hasAction(Intent.ACTION_VIEW),
+            hasData(uri)
+        )
+        Intents.intending(intent).respondWith(Instrumentation.ActivityResult(0, null))
+        action()
+        intended(intent)
         Intents.release()
     }
 }
